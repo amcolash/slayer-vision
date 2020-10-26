7b6a81918bdd65343dbebc5af7192adf320a5a86
Andrew McOlash <amcolash@gmail.com>
2020-10-26 01:56:01 -0700

    bonus detection, highlight coins, multiple coin images

--- test.py
+++ test.py
@@ -9,15 +9,18 @@ def resize(path):
   img = cv2.imread(path, 0)
   return cv2.resize(img, (int(img.shape[1] * scale), int(img.shape[0] * scale)))
 
-def find(img_rgb, img_gray, draw_img, template, threshold = 0.75):
+def find(img_rgb, img_gray, draw_img, templates, threshold = 0.75):
   found = []
-  w, h = template.shape[::-1]
-  res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-  loc = np.where( res >= threshold)
-  for pt in zip(*loc[::-1]):
-    point = [pt, (pt[0] + w, pt[1] + h)]
-    cv2.rectangle(draw_img, point[0], point[1], (0,0,255), 2)
-    found.append(point)
+
+  for template in templates:
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+      point = [pt, (pt[0] + w, pt[1] + h)]
+      cv2.rectangle(draw_img, point[0], point[1], (0,0,255), 2)
+      found.append(point)
+
   return found
 
 
@@ -45,13 +48,15 @@ monitor = {"top": y, "left": x, "width": w, "height": h}
 # scale and load test images
 scale = ((w / fullW) + (h / fullH)) / 2
 
-coin = resize('images/coin_1.png')
-bee = resize('images/bee_1.png')
-player = resize('images/player_1.png')
-boost_img = resize('images/boost.png')
+coin1 = resize('images/coin_1.png')
+coin2 = resize('images/coin_2.png')
+coin3 = resize('images/coin_3.png')
 orange_block = resize('images/orange_block.png')
+boost_img = resize('images/boost.png')
+bonus_img = resize('images/bonus.png')
 
 boost_timeout = time.time() - 15
+bonus_count = 0
 
 with mss() as sct:
   while(True):
@@ -59,9 +64,9 @@ with mss() as sct:
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     draw_img = img_rgb.copy()
 
-    coins = find(img_rgb, img_gray, draw_img, coin, 0.5)
-    block = find(img_rgb, img_gray, draw_img, orange_block)
-    boost = find(img_rgb, img_gray, draw_img, boost_img, 0.65)
+    coins = find(img_rgb, img_gray, draw_img, [coin1, coin2], 0.5)
+    block = find(img_rgb, img_gray, draw_img, [orange_block])
+    boost = find(img_rgb, img_gray, draw_img, [boost_img], 0.65)
 
     should_jump = False
     is_boost = time.time() - boost_timeout < 10
@@ -69,7 +74,7 @@ with mss() as sct:
 
     # TODO: make thresholds relative to scale
     coin_thresh_x_left = 60
-    coin_thresh_x_right = 150
+    coin_thresh_x_right = 180
     coin_thresh_x_boost = int(coin_thresh_x_right + 225 * (1 - (time.time() - boost_timeout) / 10))
     coin_thresh_y = 175
     cv2.line(draw_img, (coin_thresh_x_left, 0), (coin_thresh_x_left, h), (0,0,255), 2)
@@ -77,14 +82,12 @@ with mss() as sct:
     cv2.line(draw_img, (0, coin_thresh_y), (w, coin_thresh_y), (0,0,255), 2)
 
     if is_boost:
-      print(coin_thresh_x_boost)
       cv2.line(draw_img, (coin_thresh_x_boost, 0), (coin_thresh_x_boost, h), (0,0,255), 2)
 
     for c in coins:
-      if c[0][0] > coin_thresh_x_left:
-        if is_boost and c[0][0] < coin_thresh_x_boost:
-          should_jump = True
-        elif c[0][0] < coin_thresh_x_right:
+      if c[0][0] > coin_thresh_x_left and c[0][1] < coin_thresh_y:
+        if (is_boost and c[0][0] < coin_thresh_x_boost) or c[0][0] < coin_thresh_x_right:
+          cv2.rectangle(draw_img, c[0], c[1], (0,255,0), 2)
           should_jump = True
 
     block_thresh = 100
@@ -92,18 +95,28 @@ with mss() as sct:
 
     for b in block:
       if b[0][0] < block_thresh:
+        cv2.rectangle(draw_img, b[0], b[1], (255,0,0), 2)
         should_jump = True
 
     # should_jump = True
     if should_jump:
       subprocess.check_output(["xdotool", "mousemove", str(int(x + w / 2)), str(int(y + h / 2)), "mousedown", "1", "sleep", "0.15", "mouseup", "1"])
 
+    # TODO: make this work better since coins reduce cooldown
     # only boost when there is not a frenzy
     if len(coins) < 16 and len(boost) > 0 and boost_enabled:
-      print('boostin')
       b = boost[0]
       subprocess.check_output(["xdotool", "mousemove", str(int(x + (b[0][0] + b[1][0]) / 2)), str(int(y + (b[0][1] + b[1][1]) / 2)), "click", "1"])
       boost_timeout = time.time()
+
+    # For now, just run the bonus and fail
+    bonus_count = bonus_count + 1
+    if bonus_count > 30:
+      bonus_count = 0
+      bonus = find(img_rgb, img_gray, draw_img, [bonus_img])
+      if len(bonus) > 0:
+        b = bonus[0]
+        subprocess.check_output(["xdotool", "mousemove", str(int(x + (b[0][0] + b[1][0]) / 2)), str(int(y + (b[0][1] + b[1][1]) / 2)), "click", "1"])
 
     cv2.imshow('frame', draw_img)
 
